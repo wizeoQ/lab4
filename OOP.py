45e54c08c4fede712e5eea4f8c92d45414bfe383
wizeoQ <quartetqoc@gmail.com>
2026-05-02 21:19:53 +0300

    Освоить самостоятельно инкапсуляцию

--- OOP.py
+++ OOP.py
@@ -1,66 +1,50 @@
 import wizeo_lib as wiz
-
+from typing import Union
 
 # Инкапсуляция
-class BankAccount:
-    def __init__(self, account_number, balance):
-        # Публичный атрибут
-        self.account_number = account_number
-        # Приватный атрибут
-        self.__balance = balance
-
-
-    def deposit(self, amount):
-        # Метод для увеличения баланса
-        self.__balance += amount
-        print(f"Внесено {amount}. Баланс: {self.__balance}")
-
-
-    def get_balance(self):
-        # Метод для получения текущего баланса
-        return self.__balance
-
-
-# Пример использования
-account = BankAccount("88005553535", 20926)
-account.deposit(67)
-print(account.get_balance())
-# account.__balance #Ошибка: приватный аттрибут не выведется.
-
 wiz.l()
-# Наследование
-class Animal:
-    def __init__(self, name):
-        # Общий атрибут для всех животных
-        self.name = name
+class Vehicle:
+    def __init__(self, 
+                 speed: Union[int,float], 
+                 fuel: float) -> None:
+        self.speed = speed
+        if speed < 0: raise ValueError ("Значение скорости некорректно.")
+        self.__fuel = fuel
+        if not (0 <= fuel <=100):
+            raise ValueError ("Значение топлива некорректно.")
+            
+    def get_speed(self):
+        if self.__fuel == 0:
+            self.speed = 0
+        print(f"Скорость - {self.speed}")
 
-
-    def speak(self):
-        # Базовый метод: Общий для всех животных.
-        return f"{self.name} произносит звук"
+    def get_fuel(self):
+        print(f"Бак заполнен на {self.__fuel}%")
     
-class Dog(Animal):
-    def speak(self):
-        # Переопредление метода для класса Dog
-        return f"{self.name} лает."
-
-class Cat(Animal):
-    def speak(self):
-        # Переопределение метода для класса Cat
-        return f"{self.name} мурчит."
-# Пример использования
-dog = Dog("Кот")
-cat = Cat("Собака")
-print(dog.speak())
-print(cat.speak())
-
-# Полиморфизм
-def animal_sound(animal):
-    # Метод вызывает speak(), независимо от типа животного
-    print(animal.speak())
+    def waste(self, amount):
+        self.__fuel -= amount
+        if self.__fuel < 0:
+            self.__fuel = 0
+        print(f"Бензин потрачен на {amount}%\
+              "f"\nБак заполнен на {self.__fuel}%")
+        
+    def set_fuel(self):
+        self.__fuel = 100
+        print("Бак заполнен")
 
 
-# Полиморфизм: вызов одинакового метода для разных объектов
+car = Vehicle(120, 100)
+car.get_fuel()
+car.get_speed()
+car.waste(10)
+car.waste(20)
+car.waste(30)
+car.waste(40)
+car.get_speed()
+car.set_fuel()
+car.speed = 230
+car.get_speed()
+car.__fuel = 70
+car.get_fuel()
+
 wiz.l()
-animal_sound(dog)
-animal_sound(cat)
