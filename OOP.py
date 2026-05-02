82acf5e7e7f77cada459f75126aa1927b389ba56
wizeoQ <quartetqoc@gmail.com>
2026-05-02 19:12:37 +0300

    Добавить пример наследования в код

--- OOP.py
+++ OOP.py
@@ -1,3 +1,7 @@
+import wizeo_lib as wiz
+
+
+# Инкапсуляция
 class BankAccount:
     def __init__(self, account_number, balance):
         # Публичный атрибут
@@ -22,3 +26,30 @@ account = BankAccount("88005553535", 20926)
 account.deposit(67)
 print(account.get_balance())
 # account.__balance #Ошибка: приватный аттрибут не выведется.
+
+wiz.l()
+# Наследование
+class Animal:
+    def __init__(self, name):
+        # Общий атрибут для всех животных
+        self.name = name
+
+
+    def speak(self):
+        # Базовый метод: Общий для всех животных.
+        return f"{self.name} произносит звук"
+    
+class Dog(Animal):
+    def speak(self):
+        # Переопредление метода для класса Dog
+        return f"{self.name} лает."
+
+class Cat(Animal):
+    def speak(self):
+        # Переопределение метода для класса Cat
+        return f"{self.name} мурчит."
+# Пример использования
+dog = Dog("Кот")
+cat = Cat("Собака")
+print(dog.speak())
+print(cat.speak())
