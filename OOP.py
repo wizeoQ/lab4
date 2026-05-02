1e5e93b85999bb1264b23c7bfa7afd755a3f2ddf
wizeoQ <quartetqoc@gmail.com>
2026-05-02 19:00:26 +0300

    Добавить пример инкапсуляции в код

--- OOP.py
+++ OOP.py
@@ -1,1 +1,24 @@
-print("И восстали машины из пепла ядерного огня...")
+class BankAccount:
+    def __init__(self, account_number, balance):
+        # Публичный атрибут
+        self.account_number = account_number
+        # Приватный атрибут
+        self.__balance = balance
+
+
+    def deposit(self, amount):
+        # Метод для увеличения баланса
+        self.__balance += amount
+        print(f"Внесено {amount}. Баланс: {self.__balance}")
+
+
+    def get_balance(self):
+        # Метод для получения текущего баланса
+        return self.__balance
+
+
+# Пример использования
+account = BankAccount("88005553535", 20926)
+account.deposit(67)
+print(account.get_balance())
+# account.__balance #Ошибка: приватный аттрибут не выведется.
